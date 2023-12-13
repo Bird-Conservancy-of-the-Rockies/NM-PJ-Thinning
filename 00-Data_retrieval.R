27e62b75e611269bdd9e97e47b8845d0241b746e
Quresh Latif <qureshlatif@yahoo.com>
2023-12-13 08:51:13 -0700

    add code for data retrieval

---
+++ 00-Data_retrieval.R
@@ -0,0 +1,6 @@
+library(dplyr)
+library(GetDataBCR)
+
+SampUnits <- SampleUnits(Stratum.filter = c("NM-PJ-TR", "NM-PJ-CO"))
+BirdData <- BirdData(Stratum.filter = c("NM-PJ-TR", "NM-PJ-CO"))
+BirdDets %>% select(BirdCode, Species) %>% distinct() %>% View()
