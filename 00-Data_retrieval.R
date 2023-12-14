d4c115f2697cf29b513fed90541c298c29a9f042
Quresh Latif <qureshlatif@yahoo.com>
2023-12-14 07:50:11 -0700

    Update 00-Data_retrieval.R
    Correct error in naming 'BirdDets'.

--- 00-Data_retrieval.R
+++ 00-Data_retrieval.R
@@ -2,5 +2,5 @@ library(dplyr)
 library(GetDataBCR)
 
 SampUnits <- SampleUnits(Stratum.filter = c("NM-PJ-TR", "NM-PJ-CO"))
-BirdData <- BirdData(Stratum.filter = c("NM-PJ-TR", "NM-PJ-CO"))
+BirdDets <- BirdData(Stratum.filter = c("NM-PJ-TR", "NM-PJ-CO"))
 BirdDets %>% select(BirdCode, Species) %>% distinct() %>% View()
